16688df485c1947555bf3b0880552a67c4230977
AtakTalay <talay.yucel@ug.bilkent.edu.tr>
2024-05-02 11:18:59 +0300

    Docs and gitignore configs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
+add_module_names = False
+autodoc_member_order = 'bysource'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
